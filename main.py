f8c822c1327fb3664d548f64c57033605fa998ec
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-03-17 07:34:47 -0400

    Created card class, deck class, and made ranks

---
+++ main.py
@@ -0,0 +1,20 @@
+# need to create a deck of cards a have player/dealer hands
+
+# deal the cards
+ 
+# program needs to calculate the value of each hand
+
+# compare dealer hand to player hand to determine the winner
+
+# and the game loop
+
+class Card:
+    def __init__(self, rank):
+        self.rank = rank
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        for rank in ranks:
+            self.card.append(Card(rank))
