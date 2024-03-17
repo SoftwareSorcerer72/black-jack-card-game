afd841e66196b63d9a9827351a3376a055d61e28
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-03-17 09:13:13 -0400

    adding functionality to gui and added a background color and size dimensions to the window

--- gui.py
+++ gui.py
@@ -5,7 +5,30 @@ class BlackjackGui:
         self.root = root
         root.title("Blackjack")
 
+        # sets window size
+        root.geometry("800x600")
+
+        # sets background color
+        root.configure(bg='green')
+
         self.player_label = tk.Label(root, text="Your hand:")
         self.player_label.pack()
 
-        
+        self.dealer_label = tk.Label(root, text="Dealer's hand:")
+        self.dealer_label.pack()
+
+        self.hit_button = tk.Button(root, text="Hit", command=self.hit)
+        self.hit_button.pack()
+
+        self.stand_button = tk.Button(root, text="Stand", command=self.stand)
+        self.stand_button.pack()
+
+    def hit(self):
+        pass
+
+    def stand(self):
+        pass
+
+root = tk.Tk()
+gui = BlackjackGui(root)
+root.mainloop()
